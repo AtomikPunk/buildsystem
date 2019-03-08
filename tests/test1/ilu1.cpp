727ceecfe780cb63eb5d248512378723879d3f37
Atomik Punk <atomikp@gmail.com>
2019-03-08 14:51:53 -0500

    Add test for import libs...

---
+++ tests/test1/ilu1.cpp
@@ -0,0 +1,8 @@
+#include "implib1.h"
+
+int main(int argc, char *argv[])
+{
+	implib1(123);
+
+	return 0;
+}
---
+++ tests/test1/implib1.cpp
@@ -0,0 +1,7 @@
+#include "implib1.h"
+#include <iostream>
+
+void implib1(int val)
+{
+	std::cout << "implib1: " << val << std::endl;
+}
