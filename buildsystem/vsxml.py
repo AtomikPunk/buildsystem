bcb7e9a71f59606e16caee2b5417c95ebe9129be
Atomik Punk <atomikp@gmail.com>
2019-03-19 00:05:24 -0400

    Working roundtrip xml reading and writing of sample vcxproj file.

--- buildsystem/vsxml.py
+++ buildsystem/vsxml.py
@@ -63,12 +63,13 @@ class attribute(object):
 class element(object):
 	def __init__(self, name):
 		self.children = []
-		self.attributes = []
+		self.attributes = {}
 		self.name = name
 
 class vsxml2reader(object):
 	def __init__(self):
 		self.str = None
+		self.parsedindex = None
 
 	def read(self, fileName):
 		with open(fileName) as f:
@@ -79,19 +80,20 @@ class vsxml2reader(object):
 
 		# Skip XML declaration
 		if self.str.find('<?', index) != -1:
-			index = self.str.find('?>') + 2
+			self.parsedindex = self.str.find('?>') + 2
 
 		# Root element
-		idx = self.str.find('<', index)
+		idx = self.str.find('<', self.parsedindex)
 		rootnode = self.parseelement(idx)
 		
 		return rootnode
 
 	def parseelement(self, beg):
 		idx = self.locateelement(beg)
+		self.parsedindex = idx[1]
 		components = self.str[idx[0] + 1 : idx[1] - 1].split(' ')
 		name = components.pop(0)
-		print('vsxml: ' + name)
+		#print('vsxml: ' + name)
 		if not len(components):
 			empty = False
 		else:
@@ -106,18 +108,25 @@ class vsxml2reader(object):
 		if empty:
 			return e
 
-		nextidx = self.locateelement(idx[1])
-		while nextidx is not None:
-			if self.str[nextidx[0] + 1] == '/':
-				tag = self.str[nextidx[0] + 2 : nextidx[1] - 1]
+		idx = self.locateelement(idx[1] + 1)
+		while idx is not None:
+			# Text content
+			txt = self.str[self.parsedindex : idx[0]].strip()
+			if txt:
+				e.children.append(txt)
+
+			self.parsedindex = idx[1]
+			if self.str[idx[0] + 1] == '/':
+				tag = self.str[idx[0] + 2 : idx[1] - 1]
 				if tag == e.name:
 					# Closing current element
+					#print('vsxml: /' + tag)
 					return e
 
-				raise ValueError('vsxml error: invalid closing element at index ' + idx[nextidx[0]] + ', expected </' + e.name + '>')
+				raise ValueError('vsxml error: invalid closing element at index ' + str(idx[0]) + ', expected </' + e.name + '>')
 			else:
-				e.children.append(self.parseelement(nextidx[0]))
-			nextidx = self.locateelement(nextidx[1])
+				e.children.append(self.parseelement(idx[0]))
+			idx = self.locateelement(self.parsedindex + 1)
 
 		return e
 
@@ -143,52 +152,52 @@ class vsxml2reader(object):
 			attr[key] = value
 		return attr
 
-	def tostring(self, node):
-		return s
+class vsxml2writer(object):
+	def __init__(self):
+		self.indentlevel = 0
+		self.indentsymbol = '  '
 
 	def write(self, fileName, node):
 		with open(fileName, 'w') as f:
 			f.write(self.tostring(node))
 
-	def writeEmptyEl(self, tag, attr = []):
-		self.stream.write(self.indentsymbol*self.currentlevel)
-		self.stream.write('<' + tag)
-		for a in attr:
-		   self.stream.write(' ' + a[0] + '="' + a[1] + '"')
-		self.stream.write(' />\n')
+	def tostring(self, node):
+		s = ''
+		s += self.prolog()
+		s += self.elementtostring(node)
+		return s
 
-	def writeDeclaration(self):
-		self.write('<?xml version="1.0" encoding="utf-8"?>')
+	def elementtostring(self, node):
+		s = ''
+		s += self.indentlevel*self.indentsymbol + '<' + node.name
+		for k,v in node.attributes.items():
+			s += ' ' + k + '="' + v + '"'
+		if node.children:
+			s += '>'
+			self.indentlevel += 1
+			haselement = False
+			for c in node.children:
+				if isinstance(c, str):
+					# text
+					s += c
+				elif isinstance(c, element):
+					# element
+					s += '\n' + self.elementtostring(c)
+					haselement = True
+			self.indentlevel -= 1
+			if haselement:
+				s += '\n' + self.indentlevel*self.indentsymbol
+			s += '</' + node.name + '>'
+		else:
+			s += ' />'
+		return s
 
-	def el(self, tag, attr = [], inline = False):
-		self.tag.append(tag)
-		self.inline.append(inline)
-		self.attr = attr
-		return self
-
-	def __enter__(self):
-		# Opening tag
-		#if not self.inline[-1]:
-		self.stream.write(self.indentsymbol*self.currentlevel)
-		self.stream.write('<' + self.tag[-1])
-		for a in self.attr:
-		   self.stream.write(' ' + a[0] + '="' + a[1] + '"')
-		self.stream.write('>')
-		if not self.inline[-1]:
-			self.stream.write('\n')
-		self.currentlevel += 1
-		return self
-
-	def __exit__(self, type, value, traceback):
-		# Closing tag
-		self.currentlevel -= 1
-		if not self.inline[-1]:
-			self.stream.write(self.indentsymbol*self.currentlevel)
-		self.stream.write('</' + self.tag[-1] + '>')
-		self.stream.write('\n')
-		self.tag.pop()
-		self.inline.pop()
+	def prolog(self):
+		return '<?xml version="1.0" encoding="utf-8"?>\n'
 
 if __name__ == "__main__":
 	r = vsxml2reader()
 	xml = r.read('test.vcxproj')
+
+	w = vsxml2writer()
+	w.write('testwrite.vcxproj', xml)
